d74b338a5c06284df3b54653fc54e801414b964f
2dda9oo <134487562+2dda9oo@users.noreply.github.com>
2024-03-14 17:20:32 +0900

    [level 2] Title: 더 맵게, Time: 2603.03 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int a : scoville) {
+            pq.add(a);
+        }
+        int min = pq.peek();
+        
+        while(min < K) {
+            if(pq.size() >= 2) {
+                answer++;
+                pq.add(pq.poll() + (pq.poll() * 2));
+                min = pq.peek();
+            } else {
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
